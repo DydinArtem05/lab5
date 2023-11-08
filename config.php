60279152f4e42d2bf6c3e499eaf801fb5120b2fc
unknown <you@example.com>
2023-11-08 20:24:21 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$users = array(
+    array(
+        'login' => 'user1',
+        'password' => 'password1',
+        'name' => 'Користувач 1'
+    ),
+    array(
+        'login' => 'user2',
+        'password' => 'password2',
+        'name' => 'Користувач 2'
+    ),
+);
+?>
